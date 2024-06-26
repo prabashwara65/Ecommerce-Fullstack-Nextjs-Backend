f4e961e7d42a3c20b509be9224673e71ce936232
Priyanka Prabashwara <63540465+prabashwara65@users.noreply.github.com>
2024-06-26 16:30:36 +0530

    Update Products.js

--- models/Products.js
+++ models/Products.js
@@ -4,8 +4,7 @@ const { Schema, model } = require("mongoose");
 
 const ProductScehema = new Schema({
     title: {type: String , required: true},
-    description: String,
-    price: {type: Number , required: true},
+    
     images: [{type: String}],
     category: {type:mongoose.Types.ObjectId, ref:'Category'},
     properties: {type: Object},
