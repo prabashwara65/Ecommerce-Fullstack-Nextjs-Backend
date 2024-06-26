bc8a677d5bbd2533f3cf2717aafacd5f8759de2d
Priyanka Prabashwara <63540465+prabashwara65@users.noreply.github.com>
2024-06-26 16:34:18 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,13 +11,7 @@ export default function Home() {
             <h2>
                 Hello, <b>{session?.user?.name}</b>
             </h2>
-            <div className="flex bg-gray-300 text-balck gap-1 rounded-lg overflow-hidden">
-                <img src={session?.user?.image} alt="profile-image" className="w-6 h-6" />
-                <span className="py-1 px-2">
-                    {session?.user?.name}
-                </span>
-
-            </div>
+            
 
         </div>
 
